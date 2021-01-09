eed8d17e365601c51f2271d2202a5a898625f516
Joseph LaMantia Jr <40052124+JosephLaMantia@users.noreply.github.com>
2021-01-09 13:01:37 -0500

    Created a more accessible grid using a table, and created addRow method

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
-const container = document.getElementById("container");
+// initial num of rows and columns
+// incremented when "add" functions are called, in order to keep track of grid
+let numOfRows = 1;
+let numOfCols = 1;
 
-function makeRows(rows, cols) {
-  container.style.setProperty('--grid-rows', rows);
-  container.style.setProperty('--grid-cols', cols);
-  for (c = 0; c < (rows * cols); c++) {
-    let cell = document.createElement("div");
-    cell.innerText = (c + 1);
-    container.appendChild(cell).className = "grid-item";
-  };
-};
+// addRow: Allows user to add another row to our grid
+function addRow(){
+    let grid = document.getElementById('grid');
+    let newRow = document.createElement("tr");  //creates row
 
-makeRows(3, 3);
+
+    for(let i = 0; i < numOfCols; i++) {    //fills the row based on number of columns
+        let cell = document.createElement("td");
+        newRow.appendChild(cell);
+    }
+    grid.appendChild(newRow);   //adds the created row to our grid
+    numOfRows++;    //increments number of rows for future reference
+}
+
