e1404c4c1aef539dff52d528406e71269d1079c5
Joseph LaMantia Jr <40052124+JosephLaMantia@users.noreply.github.com>
2021-01-09 23:47:09 -0500

    added dropdown to change background color. closes #7

--- script.js
+++ script.js
@@ -61,20 +61,27 @@ function removeCol(){
 
 /* When the user clicks on the button,
 toggle between hiding and showing the dropdown content */
-function myFunction() {
+function myDropdown() {
     document.getElementById("myDropdown").classList.toggle("show");
   }
-  
   // Close the dropdown menu if the user clicks outside of it
   window.onclick = function(event) {
     if (!event.target.matches('.dropbtn')) {
-      var dropdowns = document.getElementsByClassName("dropdown-content");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
+      let dropdowns = document.getElementsByClassName("dropdown-content");
+      let i;
+      for (let i = 0; i < dropdowns.length; i++) {
+        let openDropdown = dropdowns[i];
         if (openDropdown.classList.contains('show')) {
           openDropdown.classList.remove('show');
         }
       }
     }
+  }
+
+  function goldColor(){
+    document.body.style.backgroundColor = "gold";
+  }
+
+  function whiteColor(){
+    document.body.style.backgroundColor = "white";
   }
